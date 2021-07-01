50981ad1cdeda9aec8e318784e630484e47f1c2d
Hidoni <ron30086@walla.com>
2021-07-01 22:24:39 +0300

    import optimization

--- src/main/java/com/hidoni/customizableelytra/mixin/DownloadingTextureInvoker.java
+++ src/main/java/com/hidoni/customizableelytra/mixin/DownloadingTextureInvoker.java
@@ -3,10 +3,8 @@ package com.hidoni.customizableelytra.mixin;
 import net.minecraft.client.renderer.texture.DownloadingTexture;
 import net.minecraft.client.renderer.texture.NativeImage;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
-import java.io.File;
 import java.io.InputStream;
 
 @Mixin(DownloadingTexture.class)
