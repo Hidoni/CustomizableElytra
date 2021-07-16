40565447f31432faf105d671b8b36da617844cad
Hidoni <ron30086@walla.com>
2021-07-16 18:50:01 +0300

    Elytra tag is now created if caelus hasnt already done so

--- src/main/java/com/hidoni/customizableelytra/CustomizableElytra.java
+++ src/main/java/com/hidoni/customizableelytra/CustomizableElytra.java
@@ -5,11 +5,14 @@ import com.hidoni.customizableelytra.events.ClientEventHandler;
 import com.hidoni.customizableelytra.events.ElytraRenderHandler;
 import com.hidoni.customizableelytra.events.EntityConstructingHandler;
 import com.hidoni.customizableelytra.setup.Registration;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.ForgeTagHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -37,6 +40,11 @@ public class CustomizableElytra
         {
             caelusLoaded = true;
             MinecraftForge.EVENT_BUS.register(new ElytraRenderHandler());
+        }
+        else
+        {
+            // If Caelus isn't loaded, this tag needs to be created to prevent crashing
+            ForgeTagHandler.createOptionalTag(ForgeRegistries.ITEMS, new ResourceLocation("forge", "elytra"));
         }
         curiosLoaded = ModList.get().isLoaded("curios");
         MinecraftForge.EVENT_BUS.register(new EntityConstructingHandler());
