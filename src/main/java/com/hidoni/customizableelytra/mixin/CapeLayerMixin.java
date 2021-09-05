db3e12a368abfcaaee2fc8297d222feee6135a9f
Hidoni <ron30086@walla.com>
2021-09-06 02:56:45 +0300

    Fixed logic that hides capes to hide cape when elytra is found in any valid slot

--- src/main/java/com/hidoni/customizableelytra/mixin/CapeLayerMixin.java
+++ src/main/java/com/hidoni/customizableelytra/mixin/CapeLayerMixin.java
@@ -1,11 +1,11 @@
 package com.hidoni.customizableelytra.mixin;
 
 import com.hidoni.customizableelytra.setup.ModItems;
+import com.hidoni.customizableelytra.util.ElytraInventoryUtil;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.entity.player.AbstractClientPlayerEntity;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.entity.layers.CapeLayer;
-import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class CapeLayerMixin {
     @Inject(at = @At(value = "HEAD"), method = "render(Lcom/mojang/blaze3d/matrix/MatrixStack;Lnet/minecraft/client/renderer/IRenderTypeBuffer;ILnet/minecraft/client/entity/player/AbstractClientPlayerEntity;FFFFFF)V", cancellable = true)
     public void preRenderCustomizableElytraCheck(MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn, AbstractClientPlayerEntity entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, CallbackInfo ci) {
-        ItemStack itemstack = entitylivingbaseIn.getItemStackFromSlot(EquipmentSlotType.CHEST);
+        ItemStack itemstack = ElytraInventoryUtil.tryFindElytra(entitylivingbaseIn);
         if (itemstack.getItem() == ModItems.CUSTOMIZABLE_ELYTRA.get()) {
             ci.cancel();
         }
