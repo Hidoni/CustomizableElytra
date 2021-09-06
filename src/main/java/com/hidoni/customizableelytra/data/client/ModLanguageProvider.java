6395f877e5db8ae3a99884084ec3a938995cb610
Hidoni <ron30086@walla.com>
2021-09-06 16:04:37 +0300

    Added translation keys for Base pattern

--- src/main/java/com/hidoni/customizableelytra/data/client/ModLanguageProvider.java
+++ src/main/java/com/hidoni/customizableelytra/data/client/ModLanguageProvider.java
@@ -3,7 +3,11 @@ package com.hidoni.customizableelytra.data.client;
 import com.hidoni.customizableelytra.CustomizableElytra;
 import com.hidoni.customizableelytra.items.CustomizableElytraItem;
 import com.hidoni.customizableelytra.setup.ModItems;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.item.DyeColor;
+import net.minecraft.tileentity.BannerPattern;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.common.data.LanguageProvider;
 
 public class ModLanguageProvider extends LanguageProvider {
@@ -17,5 +21,8 @@ public class ModLanguageProvider extends LanguageProvider {
         add(CustomizableElytraItem.LEFT_WING_TRANSLATION_KEY, "Left Wing:");
         add(CustomizableElytraItem.RIGHT_WING_TRANSLATION_KEY, "Right Wing:");
         add(CustomizableElytraItem.HIDDEN_CAPE_TRANSLATION_KEY, "Cape Pattern Hidden");
+        for (DyeColor dye : DyeColor.values()) {
+            add("block.minecraft.banner." + BannerPattern.BASE.getFileName() + '.' + dye.getTranslationKey(), I18n.format("item.minecraft.firework_star." + dye.getTranslationKey()) + " Base");
+        }
     }
 }
