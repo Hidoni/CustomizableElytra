c1ed021cbd17cfb47530c576bc0039edc25e1672
Hidoni <ron30086@walla.com>
2021-09-14 16:47:49 +0300

    Removed old method of adding layers to players and armor stands

--- src/main/java/com/hidoni/customizableelytra/events/ClientEventHandler.java
+++ src/main/java/com/hidoni/customizableelytra/events/ClientEventHandler.java
@@ -4,29 +4,15 @@ import com.hidoni.customizableelytra.items.CustomizableElytraItem;
 import com.hidoni.customizableelytra.renderers.CustomizableElytraLayer;
 import com.hidoni.customizableelytra.setup.ModItems;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.player.PlayerRenderer;
-import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.client.renderer.item.ItemPropertyFunction;
 import net.minecraft.client.renderer.item.ItemProperties;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-
-import javax.annotation.Nullable;
 
 public class ClientEventHandler {
     public static void handleClientLoading(final FMLClientSetupEvent event) {
-        Minecraft minecraft = Minecraft.getInstance();
-        for (PlayerRenderer renderer : minecraft.getEntityRenderDispatcher().getSkinMap().values()) {
-            renderer.addLayer(new CustomizableElytraLayer<>(renderer));
-        }
-
-        ItemProperties.register(ModItems.CUSTOMIZABLE_ELYTRA.get(), new ResourceLocation("broken_elytra"), new ItemPropertyFunction() {
-            @Override
-            public float call(ItemStack stack, @Nullable ClientLevel world, @Nullable LivingEntity entity) {
-                return CustomizableElytraItem.isFlyEnabled(stack) ? 0 : 1;
-            }
-        });
+        ItemProperties.register(ModItems.CUSTOMIZABLE_ELYTRA.get(), new ResourceLocation("broken_elytra"), (stack, world, entity, i) -> CustomizableElytraItem.isFlyEnabled(stack) ? 0 : 1);
     }
 }
