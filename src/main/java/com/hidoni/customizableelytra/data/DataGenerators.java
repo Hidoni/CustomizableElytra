b9ac41e8c0896b052ebaf6944acf664263312374
Hidoni <ron30086@walla.com>
2022-08-13 19:35:39 +0300

    Update to Forge 41.1.0

--- src/main/java/com/hidoni/customizableelytra/data/DataGenerators.java
+++ src/main/java/com/hidoni/customizableelytra/data/DataGenerators.java
@@ -5,9 +5,9 @@ import com.hidoni.customizableelytra.data.client.ModItemModelProvider;
 import com.hidoni.customizableelytra.data.client.ModLanguageProvider;
 import net.minecraft.data.DataGenerator;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
 
 @Mod.EventBusSubscriber(modid = CustomizableElytra.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DataGenerators {
--- src/main/java/com/hidoni/customizableelytra/events/ItemColorHandler.java
+++ src/main/java/com/hidoni/customizableelytra/events/ItemColorHandler.java
@@ -5,18 +5,18 @@ import com.hidoni.customizableelytra.setup.ModItems;
 import net.minecraft.core.cauldron.CauldronInteraction;
 import net.minecraft.world.item.DyeableLeatherItem;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.ColorHandlerEvent;
+import net.minecraftforge.client.event.RegisterColorHandlersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ItemColorHandler {
     @SubscribeEvent
-    public static void registerItemColor(ColorHandlerEvent.Item event) {
-        event.getItemColors().register((stack, color) ->
+    public static void registerItemColor(RegisterColorHandlersEvent.Item event) {
+        event.register((stack, color) ->
                 ((CustomizableElytraItem) stack.getItem()).getColor(stack, color), ModItems.CUSTOMIZABLE_ELYTRA.get());
 
-        event.getItemColors().register((stack, color) ->
+        event.register((stack, color) ->
                 color > 0 ? -1 : ((DyeableLeatherItem) stack.getItem()).getColor(stack), ModItems.ELYTRA_WING.get());
 
         CauldronInteraction.WATER.put(ModItems.CUSTOMIZABLE_ELYTRA.get(), CauldronInteraction.DYED_ITEM);
