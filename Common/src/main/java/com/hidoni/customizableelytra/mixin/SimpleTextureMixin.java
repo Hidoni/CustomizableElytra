ec960a4a3b05482425482d73f1d01ef2584a1086
Hidoni <ron30086@walla.com>
2023-11-01 22:51:21 +0200

    Fix SimpleTextureMixin not finding getTextureImage

--- Common/src/main/java/com/hidoni/customizableelytra/mixin/SimpleTextureMixin.java
+++ Common/src/main/java/com/hidoni/customizableelytra/mixin/SimpleTextureMixin.java
@@ -1,21 +1,41 @@
 package com.hidoni.customizableelytra.mixin;
 
+import com.hidoni.customizableelytra.Constants;
 import com.hidoni.customizableelytra.render.ImageTextureProvider;
 import net.minecraft.client.renderer.texture.SimpleTexture;
 import net.minecraft.server.packs.resources.ResourceManager;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 @Mixin(SimpleTexture.class)
 public class SimpleTextureMixin implements ImageTextureProvider {
+    @Unique
+    private Method customizableElytra$getTextureImage = null;
+
+    @Unique
+    private boolean customizableElytra$findGetTextureImageMethod() {
+        Method[] declaredMethods = SimpleTexture.class.getDeclaredMethods();
+        for (Method method : declaredMethods) {
+            if (method.getParameterTypes().length == 1 && method.getParameterTypes()[0].isAssignableFrom(ResourceManager.class) && method.getReturnType().getPackageName().equals(SimpleTexture.class.getPackageName())) {
+                customizableElytra$getTextureImage = method;
+                Constants.LOG.debug("Found getTextureImage method: " + method);
+                return true;
+            }
+        }
+        return false;
+    }
+
     @Override
     public TextureImageInvoker getImageTexture(ResourceManager resourceManager) {
         try {
-            Method getTextureImage = SimpleTexture.class.getDeclaredMethod("getTextureImage", ResourceManager.class);
-            return (TextureImageInvoker) getTextureImage.invoke(this, resourceManager);
-        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            if (customizableElytra$getTextureImage == null && !customizableElytra$findGetTextureImageMethod()) {
+                throw new RuntimeException("Failed to find getTextureImage!");
+            }
+            return (TextureImageInvoker) customizableElytra$getTextureImage.invoke(this, resourceManager);
+        } catch (InvocationTargetException | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
     }
