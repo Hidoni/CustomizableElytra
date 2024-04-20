2aa49f582ee3049c68f476e9a075615396e7c2b7
Hidoni <git@hidoni.com>
2024-04-20 14:49:52 +0300

    Prevent elytra wings from being used in the repair item recipe
    Closes #36

---
+++ Common/src/main/java/com/hidoni/customizableelytra/mixin/RepairItemRecipeMixin.java
@@ -0,0 +1,23 @@
+package com.hidoni.customizableelytra.mixin;
+
+import com.hidoni.customizableelytra.item.ElytraWingItem;
+import net.minecraft.world.inventory.CraftingContainer;
+import net.minecraft.world.item.crafting.RepairItemRecipe;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(RepairItemRecipe.class)
+public class RepairItemRecipeMixin {
+    @Inject(method = "matches(Lnet/minecraft/world/inventory/CraftingContainer;Lnet/minecraft/world/level/Level;)Z", at=@At("HEAD"), cancellable = true)
+    private void preventElytraWingRepair(CraftingContainer container, Level level, CallbackInfoReturnable<Boolean> cir) {
+        for (int i = 0; i < container.getContainerSize(); i++) {
+            if (container.getItem(i).getItem() instanceof ElytraWingItem) {
+                cir.setReturnValue(false);
+                return;
+            }
+        }
+    }
+}
