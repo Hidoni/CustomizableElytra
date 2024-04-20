645561508a1cf46d0d4f770b213e2297d5cf3eb9
Hidoni <git@hidoni.com>
2024-04-20 15:29:06 +0300

    Replace redirect in armor dyeing with injection on return
    Fixes compatibility with Cloud Storage and any other mods that might redirect the ArmorDyeRecipe assemble method
    Closes #34

--- Common/src/main/java/com/hidoni/customizableelytra/mixin/ArmorDyeRecipeMixin.java
+++ Common/src/main/java/com/hidoni/customizableelytra/mixin/ArmorDyeRecipeMixin.java
@@ -1,25 +1,24 @@
 package com.hidoni.customizableelytra.mixin;
 
 import com.hidoni.customizableelytra.item.CustomizableElytraItem;
-import net.minecraft.world.item.DyeItem;
-import net.minecraft.world.item.DyeableLeatherItem;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.ArmorDyeRecipe;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
-import java.util.List;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ArmorDyeRecipe.class)
 public class ArmorDyeRecipeMixin {
-    @Redirect(method = "assemble(Lnet/minecraft/world/inventory/CraftingContainer;Lnet/minecraft/core/RegistryAccess;)Lnet/minecraft/world/item/ItemStack;", at=@At(value = "INVOKE", target = "Lnet/minecraft/world/item/DyeableLeatherItem;dyeArmor(Lnet/minecraft/world/item/ItemStack;Ljava/util/List;)Lnet/minecraft/world/item/ItemStack;"))
-    private ItemStack handleCustomizableElytraItemDye(ItemStack stack, List<DyeItem> dyeItems) {
+    @Inject(method = "assemble(Lnet/minecraft/world/inventory/CraftingContainer;Lnet/minecraft/core/RegistryAccess;)Lnet/minecraft/world/item/ItemStack;", at=@At(value = "RETURN"), cancellable = true)
+    private void handleCustomizableElytraItemDye(CraftingContainer container, RegistryAccess registryAccess, CallbackInfoReturnable<ItemStack> cir) {
+        ItemStack stack = cir.getReturnValue();
         if (stack.getItem() instanceof CustomizableElytraItem customizableElytraItem) {
             if (!customizableElytraItem.canDye(stack)) {
-                return ItemStack.EMPTY;
+                cir.setReturnValue(ItemStack.EMPTY);
             }
         }
-        return DyeableLeatherItem.dyeArmor(stack, dyeItems);
     }
 }
