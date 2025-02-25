4059a66405397fa9d852ecbe749434ee55eecbbf
Ampflower 🌺 <git@ampflower.gay>
2025-02-24 23:34:30 -0800

    Fix: more specific field for Colytra

--- Common/src/main/java/com/hidoni/customizableelytra/mixin/ColytraLayerMixin.java
+++ Common/src/main/java/com/hidoni/customizableelytra/mixin/ColytraLayerMixin.java
@@ -48,7 +48,7 @@ public abstract class ColytraLayerMixin<T extends LivingEntity, M extends Entity
 
     @Redirect(method = "*", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/ElytraModel;renderToBuffer(Lcom/mojang/blaze3d/vertex/PoseStack;Lcom/mojang/blaze3d/vertex/VertexConsumer;III)V"))
     private void renderCustomizedElytraWings(ElytraModel<T> elytraModel, PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int overlayTexture, int argb,
-                                             @Local(argsOnly = true) MultiBufferSource buffer, @Local(argsOnly = true) LivingEntity entity, @Local ItemStack elytra) {
+                                             @Local(argsOnly = true) MultiBufferSource buffer, @Local(argsOnly = true) LivingEntity entity, @Local(name = "elytraStack") ItemStack elytra) {
         ElytraCustomization customization = CustomizationUtils.getElytraCustomization(elytra);
         if (!customization.isCustomized()) {
             elytraModel.renderToBuffer(poseStack, vertexConsumer, packedLight, overlayTexture, argb);
